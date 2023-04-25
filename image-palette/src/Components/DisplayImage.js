c54d91358de9a2806fef6b18d7335a6b16e4f975
andyholmes1210 <andy.holmes7662@gmail.com>
2023-04-26 11:03:51 +1200

    Finished Upload image section

--- image-palette/src/Components/DisplayImage.js
+++ image-palette/src/Components/DisplayImage.js
@@ -1,7 +1,18 @@
+import styles from "@/styles/Home.module.css"
 import React from "react"
 
-function DisplayImage() {
-  return <div>displayimage</div>
+function DisplayImage({ uploadedImage, colorPalette, gallery }) {
+  return (
+    <div className={styles.content}>
+      <div className="image">
+        {uploadedImage ? (
+          <img src={uploadedImage} alt="Image" />
+        ) : (
+          <h2>Image Here...</h2>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default DisplayImage
